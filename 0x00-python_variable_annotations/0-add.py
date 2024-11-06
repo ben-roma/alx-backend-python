cdaceeadca5e2f2736adc6d0d34455a8b19bfe4a
ben obame <obameben@yahoo.fr>
2024-11-06 17:49:17 +0200

    task 1

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""This module defines a type-annotated function add that takes a float a and
+a float b as arguments and returns their sum as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Adds two float numbers.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of the two float numbers.
+    """
+    return a + b
